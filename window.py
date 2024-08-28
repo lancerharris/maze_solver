5d02ef079514f19c921acda83aa6eb7909de7061
lancerharris <lancerprogram@gmail.com>
2024-08-28 16:19:01 -0700

    Fix window construction
    properly set up and pack canvas

--- window.py
+++ window.py
@@ -2,29 +2,27 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
         self._root = Tk()
-        self._root.title = "first window"
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
+        self._root.title("Maze Solver")
         self._root.protocol("WM_DELETE_WINDOW", self.close)
+        self._canvas = Canvas(self._root, bg="white", height=height, width=width)
+        self._canvas.pack(fill=BOTH, expand=1)
+        self._running = False
 
     def redraw(self):
         self._root.update_idletasks()
         self._root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running == True:
+        self._running = True
+        while self._running == True:
             self.redraw()
     
     def close(self):
-        self.running = False
+        self._running = False
 
     def draw_line(self, line, fill_color):
-        line.draw(self.canvas, fill_color)
+        line.draw(self._canvas, fill_color)
 
 class Point:
     def __init__(self, x=0, y=0):
