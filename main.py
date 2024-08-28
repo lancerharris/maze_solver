bafac796a9f375011fb4932d24191305940d7c3d
lancerharris <lancerprogram@gmail.com>
2024-08-27 18:53:10 -0700

    Add line drawing capability

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import window
 
 def main():
     win = window.Window(800, 600)
+    point1 = window.Point(100, 100)
+    point2 = window.Point(200, 200)
+    line = window.Line(point1, point2)
+    win.draw_line(line, "red")
     win.wait_for_close()
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -22,3 +22,21 @@ class Window:
     
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.x1 = point1.x
+        self.y1 = point1.y
+        self.x2 = point2.x
+        self.y2 = point2.y
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=fill_color, width = 2)
