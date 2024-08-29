985fe9452bc5327b158645297be9090a63da58f6
lancerharris <lancerprogram@gmail.com>
2024-08-29 06:04:49 -0700

    Add first unit test
    make window default to none so that tests target logic and not visuals

--- maze.py
+++ maze.py
@@ -9,7 +9,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win = None,
     ):
         self._x1 = x1
         self._y1 = y1
@@ -29,13 +29,14 @@ class Maze:
                 bottom_right_point = Point(self._x1 + self._cell_size_x * (i + 1), self._y1 + self._cell_size_y * (j + 1))
                 column.append(Cell(top_left_point, bottom_right_point, self._win))
             self._cells.append(column)
-        for i in range(self._num_cols):
-            for j in range(self._num_rows):
-                self._draw_cell(i, j)
+        if self._win:
+            for i in range(self._num_cols):
+                for j in range(self._num_rows):
+                    self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
         self._cells[i][j].draw("black")
-        if not (i == self._num_cols and j == self._num_rows):
+        if not (i == self._num_cols and j == self._num_rows) and self._win is not None:
             self._animate() 
     
     def _animate(self):
@@ -44,7 +45,7 @@ class Maze:
 
 
 class Cell:
-    def __init__(self, top_left_point, bottom_right_point, win):
+    def __init__(self, top_left_point, bottom_right_point, win = None):
         self._x1 = top_left_point.x
         self._y1 = top_left_point.y
         self._x2 = bottom_right_point.x
